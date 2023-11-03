f99dcd8749515e44aede82e2a5b77dd217af2881
Petar Petrov <ppetrov@vexelon.net>
2023-11-03 21:53:55 +0100

    Add dev debug options passed as url params

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,25 @@
+
+const isStringBoolean = (item) => item === `true`;
+
+const URLOptions = {};
+window.location.search.slice(1).split('&').map((option) => {
+  const keyValue = option.split('=');
+  URLOptions[keyValue[0]] = isStringBoolean(keyValue[1]) || keyValue[1];
+});
+
 const Consts = {
 	screenWidth: 800,
 	screenHeight: 600,
 	laneHeight: 64,
+	
+	// dev flags passed as url parameters, e.g. `?debug=true&show_fps=true`
+	debug: URLOptions[`debug`],
+	debugPhysics: URLOptions[`debug_physics`],
+	showFPS: URLOptions[`show_fps`],
+	noSounds: URLOptions[`no_sound`],
+	noMusic: URLOptions[`no_music`],
 };
+
+console.debug('[dev] options', URLOptions);
 
 export default Consts;
