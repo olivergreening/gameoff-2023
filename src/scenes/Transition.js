b9ac71b7368bfe2e9a9ea1a6c58449b465d1b6ff
Petar Petrov <ppetrov@vexelon.net>
2023-11-07 22:55:24 +0100

    Remove timer and just use the tween to count down in transition scene

--- src/scenes/Transition.js
+++ src/scenes/Transition.js
@@ -16,36 +16,42 @@ export class Transition extends Phaser.Scene {
         this.add.bitmapText(Consts.screenWidth * 0.5, Consts.screenHeight * 0.5 - 50, Consts.font, 'Get ready to escape!', 48).setOrigin(0.5);
         this.add.bitmapText(Consts.screenWidth * 0.5, Consts.screenHeight - 50, Consts.font, 'Press space to continue...', 24).setOrigin(0.5);
 
-        this.countdownText = this.add.bitmapText(Consts.screenWidth * 0.5, Consts.screenHeight - 150, Consts.font, '', 24);
+        this.countdownText = this.add.bitmapText(Consts.screenWidth * 0.5, Consts.screenHeight - 150, Consts.font, '10', 24);
         this.countdownText.setOrigin(0.5);
 
-        this.countdownTimer = this.time.addEvent({ delay: 1000, repeat: 11 });
+        this.countdown = 10;
 
         this.tween = this.tweens.addCounter({
-			from: 5,
-			to: 0,
-			duration: 1000,
-			yoyo: false,
-			repeat: 10,
-			onUpdate: (tween) => {
-                this.countdownText.scale = 1 + tween.getValue();;
-			}
-		});
+            from: 5,
+            to: 0,
+            duration: 1000,
+            yoyo: false,
+            repeat: 10,
+            onUpdate: (tween) => {
+                this.countdownText.scale = 1 + tween.getValue();
+            },
+            onRepeat: () => {
+                const count = this.countdown;
+                if (count > 1) {
+                    this.countdownText.setText(count - 1);
+                } else if (count === 1) {
+                    this.countdownText.setText('GO');
+                } else if (count === 0) {
+
+                }
+
+                this.countdown -= 1;
+            },
+            onComplete: () => {
+                this.scene.start('Game');
+            }
+        });
     }
 
     update(time, delta) {
         this.controls.update(time);
 
         if (this.controls.action1.isPressed) {
-            this.scene.start('Game');
-        }
-
-        const count = this.countdownTimer.getRepeatCount();
-        if (count > 1) {
-            this.countdownText.setText(count - 1);
-        } else if (count === 1) {
-            this.countdownText.setText('GO');
-        } else  {
             this.scene.start('Game');
         }
     }
