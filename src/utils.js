268e13ee90f62b26b46d7dd22cf3ff8856aed511
Petar Petrov <ppetrov@vexelon.net>
2023-11-21 22:31:22 +0100

    Add assert utility function

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,13 @@ import Consts from './consts';
 
 const Utils = {
 
+    assert: (what, obj) => {
+        if (typeof obj === 'undefined') {
+            throw `${what} is undefined`;
+        }
+        return obj;
+    },
+
     debug: (...args) => {
         if (Consts.debug) {
             console.debug(args);
